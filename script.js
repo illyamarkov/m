fdb4622d811702337db58834ce948d7180e5db66
Illya <illya@marqov.com>
2023-03-19 05:50:48 -0400

    new windows, css, optimization, and bug fixes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get all popup windows
 const windows = document.querySelectorAll('.window');
-const interactButton = document.querySelector('.interact');
+const interactButton = document.querySelector('.interactWindow2');
 
 
 var cimg = false;
@@ -9,18 +9,16 @@ var cimg = false;
 var totalWindows = 0;
 var wins = [];
 
-windows.forEach((window) => {
-    totalWindows += 1;
-    wins.push(window)
-});
-
-console.log(wins);
 
 // Loop through each window and add event listeners
 windows.forEach((window) => {
   // Get the title bar and the close button
   const titleBar = window.querySelector('.title-bar');
   const closeButton = window.querySelector('.close-button');
+
+  // initialize totalWindows and wins
+  totalWindows += 1;
+  wins.push(window)
 
   // Initialize variables for storing the initial position and the offset
   let initialX, initialY, offsetX, offsetY;
@@ -95,47 +93,73 @@ windows.forEach((window) => {
     window.remove();
   });
 
-  interactButton.addEventListener('click', () => {
-    // Remove the window from the DOM
-    if (getRandomInt(10001) === 5){
-        interactButton.closest('.window').querySelector('img').src = "src/!!!!!!!!!!!!.png";
-        interactButton.closest('.window').querySelector('.title').textContent = "!!!!!!!!!!!!";
-        interactButton.textContent = "!!!!!!!!!!!!";
-        cimg = true;
-    }
-
-    else if (getRandomInt(1001) === 5){
-        interactButton.closest('.window').querySelector('img').src = "src/!!!.png";
-        interactButton.closest('.window').querySelector('.title').textContent = "!!!";
-        interactButton.textContent = "!!!";
-        cimg = true;
-    }
-
-    else if (getRandomInt(101) === 5){
-        interactButton.closest('.window').querySelector('img').src = "src/FREEDOM.png";
-        interactButton.closest('.window').querySelector('.title').textContent = "?!";
-        interactButton.textContent = "?!";
-        cimg = true;
-    }
-
-    else if (cimg === false) {
-        interactButton.closest('.window').querySelector('img').src = "src/!.png";
-        interactButton.closest('.window').querySelector('.title').textContent = "!"
-        interactButton.textContent = "!";
-        cimg = true;
-    }
-    else {
-        interactButton.closest('.window').querySelector('img').src = "src/SHEPHERD.png";
-        interactButton.textContent = "?";
-        interactButton.closest('.window').querySelector('.title').textContent = "REJOICE!"
-        cimg = false;
-    }
-  });
-
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-
 
 
 });
+
+interactButton.addEventListener('click', () => {
+
+  console.log("Hello!");
+
+  let rnum = getRandomInt(10001);
+
+  if (rnum === 1) {
+    interactButton.closest('.window').querySelector('img').src = "src/!!!!!!!!!!!!.png";
+    interactButton.closest('.window').querySelector('.title').textContent = "!!!!!!!!!!!!";
+    interactButton.textContent = "!!!!!!!!!!!!";
+    cimg = true;
+  }
+
+  else if (rnum < 10){
+    interactButton.closest('.window').querySelector('img').src = "src/!!!.png";
+    interactButton.closest('.window').querySelector('.title').textContent = "!!!";
+    interactButton.textContent = "!!!";
+    cimg = true;
+  }
+
+  else if (rnum < 100){
+    interactButton.closest('.window').querySelector('img').src = "src/FREEDOM.png";
+    interactButton.closest('.window').querySelector('.title').textContent = "?!";
+    interactButton.textContent = "?!";
+    cimg = true;
+  }
+
+  else if (cimg === false) {
+    interactButton.closest('.window').querySelector('img').src = "src/!.png";
+    interactButton.closest('.window').querySelector('.title').textContent = "!"
+    interactButton.textContent = "!";
+    cimg = true;
+  }
+
+  else{
+    interactButton.closest('.window').querySelector('img').src = "src/SHEPHERD.png";
+    interactButton.textContent = "?";
+    interactButton.closest('.window').querySelector('.title').textContent = "REJOICE!"
+    cimg = false;
+  }
+
+  // Logic which generates random numbers every time:
+
+  // if (getRandomInt(10001) === 5){
+
+  // }
+
+  // else if (getRandomInt(1001) === 5){
+
+  // }
+
+  // else if (getRandomInt(101) === 5){
+
+  // }
+
+  // else if (cimg === false) {
+
+  // }
+  // else {
+      
+  // }
+});
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
